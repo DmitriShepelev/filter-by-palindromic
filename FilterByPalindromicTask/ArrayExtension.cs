89dd49682786c8aed972e83d8c35d073d4420e1e
dmitri_shepelev <shepelev.dm@gmail.com>
2020-12-23 23:28:11 +0300

    Done

--- FilterByPalindromicTask/ArrayExtension.cs
+++ FilterByPalindromicTask/ArrayExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FilterByPalindromicTask
 {
@@ -16,11 +17,42 @@ namespace FilterByPalindromicTask
         /// <exception cref="ArgumentException">Throw when array is empty.</exception>
         /// <example>
         /// {12345, 1111111112, 987654, 56, 1111111, -1111, 1, 1233321, 70, 15, 123454321}  => { 1111111, 123321, 123454321 }
-        /// {56, -1111111112, 987654, 56, 890, -1111, 543, 1233}  => {  }
+        /// {56, -1111111112, 987654, 56, 890, -1111, 543, 1233}  => {  }.
         /// </example>
         public static int[] FilterByPalindromic(int[] source)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (source is null)
+            {
+                throw new ArgumentNullException($"Array can not be null.");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException($"Array can not be empty.");
+            }
+
+            var list = new List<int>();
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] == Reverse(source[i]))
+                {
+                    list.Add(source[i]);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        private static int Reverse(int num)
+        {
+            int result = 0;
+            while (num > 0)
+            {
+                result = (result * 10) + (num % 10);
+                num /= 10;
+            }
+
+            return result;
         }
     }
 }
